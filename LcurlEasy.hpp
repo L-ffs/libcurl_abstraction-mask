91b674d2889cf0ba1388da26304a93f46fe16673
L-ffs <lffscia@gmail.com>
2026-06-16 22:35:10 -0300

    fixed request func couts, added some urls in main, updated gitignore to ignore *.ignore
     Changes to be committed:
    	modified:   .gitignore
    	modified:   LcurlEasy.hpp
    	modified:   main.cpp
    	new file:   treino.ignore

--- LcurlEasy.hpp
+++ LcurlEasy.hpp
@@ -4,55 +4,50 @@
 
 using namespace std;
 
-namespace LcurlEasy {
 
 
-    enum Metodo {GET, POST, PUT, DELETE};
+enum Metodo {GET, POST, PUT, DELETE};
 
-    class response {
-    private:
-    public:
-        long status_code;
-        CURLcode curlcode;
-        string resposta;
-        response(/* args */){}
-        ~response(){}
-    };
+class response {
+private:
+public:
+    long status_code;
+    CURLcode curlcode;
+    string resposta;
+    response(/* args */){}
+    ~response(){}
+};
 
-    size_t writefunc(void* buffer, size_t size, size_t NMemBytes, void* response){
-        size_t total= size * NMemBytes;
-        string* response_ = static_cast<string*>(response);
-        response_->append(static_cast<char*>(buffer), total);
-        return total;
-    }
-
-
-    response HTTP_req(Metodo metodo, const char* url, const char* infos= {}) {
+size_t writefunc(void* buffer, size_t size, size_t NMemBytes, void* response){
+    size_t total= size * NMemBytes;
+    string* response_ = static_cast<string*>(response);
+    response_->append(static_cast<char*>(buffer), total);
+    return total;
+}
 
 
-        if (metodo != GET) {cout << "metodo ainda nao implementado."; exit (-1);}
-        response resposta;
-        
-        CURL* curl = curl_easy_init(); //Inicializa o handle da curl 
-        if (!curl) {cerr << "Erro ao inicializar o CURL" << endl; exit(1);}
-        
-        curl_easy_setopt(curl, CURLOPT_URL, url);
-        curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, writefunc);
-        curl_easy_setopt(curl, CURLOPT_WRITEDATA, &(resposta.resposta));
+response HTTP_req(Metodo metodo, const char* url, const char* infos= {}) {
 
-        CURLcode res = curl_easy_perform(curl);
 
-        // 7. Verificação básica de erro
-        if (res != CURLE_OK) {cerr << "Falha na requisicao: " << curl_easy_strerror(res) << endl; exit(-1);}
-        
-        curl_easy_getinfo(curl, CURLINFO_RESPONSE_CODE, &resposta.status_code);
+    if (metodo != GET) {cout << "metodo ainda nao implementado."; exit (-1);}
+    response resposta;
+    
+    CURL* curl = curl_easy_init(); //Inicializa o handle da curl 
+    if (!curl) {cerr << "Erro ao inicializar o CURL" << endl; exit(1);}
+    
+    curl_easy_setopt(curl, CURLOPT_URL, url);
+    curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, writefunc);
+    curl_easy_setopt(curl, CURLOPT_WRITEDATA, &(resposta.resposta));
 
-        // Exibe os resultados na tela
-        cout << "Sucesso! Status HTTP: " << resposta.status_code << endl;
-        cout << "Seu IP Publico eh: " << resposta.resposta << endl;
+    CURLcode res = curl_easy_perform(curl);
 
-        curl_easy_cleanup(curl);
+    // 7. Verificação básica de erro
+    if (res != CURLE_OK) {cerr << "Falha na requisicao: " << curl_easy_strerror(res) << endl; exit(-1);}
+    
+    curl_easy_getinfo(curl, CURLINFO_RESPONSE_CODE, &resposta.status_code);
 
-        return resposta;
-    }
+
+
+    curl_easy_cleanup(curl);
+    return resposta;
 }
--- main.cpp
+++ main.cpp
@@ -1,15 +1,22 @@
 #include <iostream>
 #include <string>
 #include "LcurlEasy.hpp"
+
+using namespace std;
+
 const char* url_httpbin= "https://httpbin.org/get";
 const char* url_ipify= "https://api.ipify.org";
-
+const char* url_colab= "https://colab.research.google.com/drive/1zQbCbEzNrlIEuPfc843nQoWMeoaljR_z?usp=sharing#scrollTo=5rufnSrqf0rx";
 
 
 
 int main (int argc, char** argv) {
     
-    LcurlEasy::HTTP_req(LcurlEasy::GET, url_ipify);
+    response res= HTTP_req(GET, url_colab);
+
+    cout << "curl code:" << res.curlcode << endl;
+    cout << "status code:" << res.status_code << endl;
+    cout << "response body:" << res.resposta << endl;
     
     return 0;
 }
