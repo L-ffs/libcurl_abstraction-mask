ae7e2ca61b71016ca891854832599d45e2ed7349
L-ffs <lffscia@gmail.com>
2026-08-19 09:27:14 -0300

    Changes to be committed: 	modified:   .gitignore 	modified:   LcurlEasy.hpp

--- LcurlEasy.hpp
+++ LcurlEasy.hpp
@@ -4,8 +4,6 @@
 
 using namespace std;
 
-
-
 enum Metodo {GET, POST, PUT, DELETE};
 
 class response {
@@ -14,10 +12,13 @@ public:
     long status_code;
     CURLcode curlcode;
     string resposta;
-    response(/* args */){}
+    response(){}
     ~response(){}
 };
 
+/*
+    funçao que dita gerencia o registro de dados pra dentro do buffer
+*/
 size_t writefunc(void* buffer, size_t size, size_t NMemBytes, void* response){
     size_t total= size * NMemBytes;
     string* response_ = static_cast<string*>(response);
@@ -25,11 +26,8 @@ size_t writefunc(void* buffer, size_t size, size_t NMemBytes, void* response){
     return total;
 }
 
+response HTTP_req(Metodo metodo, const char* url, const char* infos = nullptr) {
 
-response HTTP_req(Metodo metodo, const char* url, const char* infos= {}) {
-
-
-    if (metodo != GET) {cout << "metodo ainda nao implementado."; exit (-1);}
     response resposta;
     
     CURL* curl = curl_easy_init(); //Inicializa o handle da curl 
@@ -39,15 +37,46 @@ response HTTP_req(Metodo metodo, const char* url, const char* infos= {}) {
     curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, writefunc);
     curl_easy_setopt(curl, CURLOPT_WRITEDATA, &(resposta.resposta));
 
-    CURLcode res = curl_easy_perform(curl);
+    // Configuração dos métodos HTTP
+    switch (metodo) {
+        
+        case GET:
+            curl_easy_setopt(curl, CURLOPT_HTTPGET, 1L);
+            break;
+            
+        case POST:
+            curl_easy_setopt(curl, CURLOPT_POST, 1L);
+            if (infos) {
+                curl_easy_setopt(curl, CURLOPT_POSTFIELDS, infos);
+            }
+            break;
+            
+        case PUT:
+            curl_easy_setopt(curl, CURLOPT_CUSTOMREQUEST, "PUT");
+            if (infos) {
+                curl_easy_setopt(curl, CURLOPT_POSTFIELDS, infos);
+            }
+            break;
+            
+        case DELETE:
+            curl_easy_setopt(curl, CURLOPT_CUSTOMREQUEST, "DELETE");
+            if (infos) {
+                curl_easy_setopt(curl, CURLOPT_POSTFIELDS, infos);
+            }
+            break;
+    }
 
-    // 7. Verificação básica de erro
+    CURLcode res = curl_easy_perform(curl);
+    
+    // Salvando o código de resultado do CURL na classe response
+    resposta.curlcode = res; 
+
+    // Verificação básica de erro
     if (res != CURLE_OK) {cerr << "Falha na requisicao: " << curl_easy_strerror(res) << endl; exit(-1);}
     
     curl_easy_getinfo(curl, CURLINFO_RESPONSE_CODE, &resposta.status_code);
 
-
-
+    // encerra o handle curl
     curl_easy_cleanup(curl);
     return resposta;
 }
